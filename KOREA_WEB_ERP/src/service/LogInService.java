a5629677c82efc6d70de5e6ec9c665fad572d322
joikho <it890122@naver.com>
2021-03-02 01:39:06 +0900

    file change

--- KOREA_WEB_ERP/src/service/LogInService.java
+++
@@ -1,9 +0,0 @@
-package service;
-
-import exception.FindException;
-import vo.Employee;
-
-public interface LogInService {
-	
-	Employee logIn(String id, String pwd) throws FindException;
-}
--- KOREA_WEB_ERP/src/vo/AnnualLeave.java
+++
@@ -1,9 +0,0 @@
-package vo;
-
-public class AnnualLeave {
-
-	public String emp_id;
-	public String apply_year;
-	public int max_day;
-	public int used_day;
-}
